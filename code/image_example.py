762d098638b69c67f0a2a25b83eb6bf680943711
ashwin123 <ashwin.naresh94@gmail.com>
2016-02-22 21:53:56 +0530

    image tagging example for both local and web urls

--- code/image_example.py
+++ code/image_example.py
@@ -2,11 +2,12 @@ from alchemyapi_python.alchemyapi import AlchemyAPI 		#add alchemyapi_python to 
 import json
 
 image_url = 'http://demo1.alchemyapi.com/images/vision/football.jpg'
-
+# image_file_url = 'C:/Users/Public/Pictures/Sample Pictures/Penguins.jpg'
 # Create the AlchemyAPI Object
 alchemyapi = AlchemyAPI()
 
 response = alchemyapi.imageTagging('url', image_url)
+# response = alchemyapi.imageTagging('image', image_file_url)
 
 if response['status'] == 'OK':
     print('## Response Object ##')
