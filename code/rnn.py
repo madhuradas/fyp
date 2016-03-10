37a2ce3188468a47d058bb1e36334f4be3471021
ashwin123 <ashwin.naresh94@gmail.com>
2016-03-10 16:45:29 +0530

    rnn skeleton (incomplete)

---
+++ code/rnn.py
@@ -0,0 +1,17 @@
+class RNN:
+	def __init__(self,):
+		pass
+
+	def train(self,):
+		pass
+
+	def predict(self,):
+		pass
+
+	def evaluate(self,):
+		pass
+
+
+if __name__ == '__main__':
+	rnn = RNN()
+	pass
