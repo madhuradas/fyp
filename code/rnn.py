52ea8a091bfd7e95bbb90dda51b32cc29ed3ac6a
ashwin123 <ashwin.naresh94@gmail.com>
2016-03-10 16:52:14 +0530

    rnn skeleton

--- code/rnn.py
+++ code/rnn.py
@@ -1,5 +1,11 @@
+import numpy as np
+import pickle 
+
 class RNN:
-	def __init__(self,):
+	def __init__(self,hidden_size,):
+		pass
+
+	def minimize(self,):
 		pass
 
 	def train(self,):
@@ -10,6 +16,18 @@ class RNN:
 
 	def evaluate(self,):
 		pass
+
+	def save(self,filename):
+		'''
+		Save model to a file
+		'''
+		pickle.dump(self,open(filename,'wb'))
+
+	def load(self,filename):
+		'''
+		Load model from file
+		'''
+		return pickle.load(open(filename))
 
 
 if __name__ == '__main__':
